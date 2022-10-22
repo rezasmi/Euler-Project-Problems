64c10693601e178ca3c87b0049d958232d59a235
Reza Sam <s_gomeishi@yahoo.com>
2022-10-22 14:03:08 +0330

    added file

---
+++ Problem9.py
@@ -0,0 +1,14 @@
+for a in range(0,400):
+    for b in range(0,400):
+        c=(1000-a)-b
+        
+        if a<b<c:
+            if a**2+b**2 ==c**2:
+                print(a,b,c)
+                print(a*b*c)
+                
+
+                
+                
+
+
