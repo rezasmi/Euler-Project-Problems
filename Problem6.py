2c1024b8331d85e74f40db150d0fcbd570a8ed89
Reza Sam <s_gomeishi@yahoo.com>
2022-10-22 14:02:54 +0330

    added file

---
+++ Problem6.py
@@ -0,0 +1,23 @@
+#def square_num(n):
+b=0
+for i in range(1,101):
+    #print(i)
+    a=i*i
+    b=a+b
+#print(b)
+
+#def square_sum(n):
+c=0
+for j in range(1,101):
+    c=c+j
+    #print(c)
+    d=c*c
+#print(d)
+print(d-b)
+#n=100
+
+#print(square_num(100))
+#square_sum(n)
+#print(str(square_sum(n)-square_num(n)))
+
+
